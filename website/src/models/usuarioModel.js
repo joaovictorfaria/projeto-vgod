e300efe91a2064d2b7b00c2e7d47164f99eff6c0
joaovictorfaria <joao.fsantana@sptech.school>
2024-06-04 10:23:13 -0300

    ajustes

--- website/src/models/usuarioModel.js
+++ website/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nome, email FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -16,7 +16,7 @@ function cadastrar(nome, sobrenome, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, sobrenome, email, senha) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}');
+        INSERT INTO Usuario (nome, sobrenome, email, senha) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -33,13 +33,13 @@ function votar(fkUsuario, voto) {
 }
 
 function listarVotos() {
-    var instrucaoSql = `select count(fkUsuario) as qtdVotos from usuario join votacao on idUsuario = fkUsuario;`;
+    var instrucaoSql = `select count(fkUsuario) as qtdVotos from Usuario join Votacao on idUsuario = fkUsuario;`;
     return database.executar(instrucaoSql);
 
   }
 
   function listarUsuarios() {
-    var instrucaoSql = `select count(idUsuario) as qtdUsuarios from usuario`;
+    var instrucaoSql = `select count(idUsuario) as qtdUsuarios from Usuario`;
     return database.executar(instrucaoSql);
 
   }
